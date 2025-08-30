41759cbec5361fa650f5f7885ed91eef7e983c91
Giuliano Maciel <giulianomaciellopes@gmail.com>
2025-08-30 10:52:22 -0300

    refactor: ultimos ajustes antes d ecriar os hooks

--- src/components/LetterPlayer/letterPlayer.tsx
+++ src/components/LetterPlayer/letterPlayer.tsx
@@ -8,7 +8,7 @@ type Props = {
   gols: string;
   LogoTeam?: string | StaticImageData;
   className?: string;
-   size?: "sm" | "md" | "lg";
+  size?:  | "md" | "lg";
 };
 
 export function LetterPlayer({
@@ -27,25 +27,36 @@ export function LetterPlayer({
       className={`relative ${s.w} ${s.h} shadow-xl border-4 border-yellow-200 overflow-hidden flex flex-col items-center
       bg-gradient-to-b from-yellow-400 via-yellow-300 to-yellow-500 clip-card ${className}`}
     >
-      {LogoTeam && (
-        <Image
-          src={LogoTeam}
-          alt="Team logo"
-          className={`absolute top-2 left-2 object-contain ${s.logo}`}
-        />
-      )}
+     
+      <div className="relative flex w-full justify-center  mt-4 h-1/2">
+        
+        {img && (
+          <Image
+            src={img}
+            alt={""}
+            width={s.playerImgWidth}
+            height={s.playerImgHeight}
+            className="rounded-full border-2 border-white object-cover shadow-md"
+          />
+        )}
 
-      {img && (
-        <Image
-          src={img}
-          alt={nome}
-          className={`rounded-full border-2 border-white object-cover shadow-md ${s.img}`}
-        />
-      )}
+       
+        {LogoTeam && (
+          <Image
+            src={LogoTeam}
+            alt="Team logo"
+            width={s.logoWidth}
+            height={s.logoHeight}
+            className="absolute top-5 left-0 object-contain"
+          />
+        )}
+      </div>
 
-      <h2 className={`mt-3 font-bold text-gray-900 ${s.text}`}>{nome}</h2>
+     
+      <h2 className={`mt-4 font-bold text-gray-900 ${s.text}`}>{nome}</h2>
 
-      <div className={`mt-4 w-full px-4 text-gray-800 font-semibold ${s.text}`}>
+      
+      <div className={`mt-2 w-full px-4 text-gray-800 font-semibold ${s.text}`}>
         <div className="flex justify-between">
           <span>Gols</span>
           <span>{gols}</span>
--- src/templates/letterPlayerPage/latterPlayerPage.tsx
+++ src/templates/letterPlayerPage/latterPlayerPage.tsx
@@ -2,8 +2,8 @@ import { LetterPlayer } from "@/components/LetterPlayer";
 
 export function LatterPlayerpage() {
   return (
-    <div className="relative w-screen h-screen flex flex-col items-center ">
-      <h1 className="md:block hidden text-heading-hg absolute z-10" >Eai campeao vai uma cartinha ai </h1>
+    <div className="relative w-screen h-screen flex flex-col items-center justify-center">
+     
       <div
         className="hidden md:flex absolute inset-0 bg-cover bg-center bg-no-repeat"
         style={{ backgroundImage: "url('/assets/fundofutebol.jpg')" }}
@@ -14,7 +14,7 @@ export function LatterPlayerpage() {
 
       {/* Card sempre visível */}
       <div className="relative z-10">
-        <LetterPlayer size="lg" assistencia="10" gols="10" nome="Giuliano" />
+        <LetterPlayer size="lg" assistencia="10" gols="10" nome="Giuliano" LogoTeam={"/logo.png"} img={"/assets/giu.jpg"} />
       </div>
     </div>
   );
